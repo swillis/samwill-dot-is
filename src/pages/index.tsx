072554070a43d07126c71d78df1741d1944e8a9f
Sam Willis <samueljohnwillis@gmail.com>
2023-07-27 09:42:48 +0100

    Progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,28 +15,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 const projects = [
   {
-    title: "Logo + brand design",
-    company: "Dependabot",
-    link: "https://dependabot.com",
-    image: "/selected-work/dependabot-dark-mode.png",
-  },
-  {
-    title: "Integration guides + API reference",
-    company: "Duffel",
-    link: "https://duffel.com/docs",
-    image: "/selected-work/docs.png",
-  },
-  {
-    title: "Homepage for gen art studio",
-    company: "gm.studio",
-    link: "https://gmstudio.art",
-    image: "/selected-work/gm-studio.png",
-  },
-  {
-    title: "Logo design",
-    company: "Incident.io",
-    link: "https://incident.io",
-    image: "/selected-work/incident-dark-mode.png",
+    title: "Gen art minting page",
+    company: "Alba",
+    link: "https://alba.art",
+    image: "/selected-work/project-page.png",
   },
   {
     title: "White-label booking flow for flights + stays",
@@ -45,16 +27,22 @@ const projects = [
     image: "/selected-work/links.png",
   },
   {
+    title: "Logo design",
+    company: "Incident.io",
+    link: "https://incident.io",
+    image: "/selected-work/incident-dark-mode.png",
+  },
+  {
     title: "Merchant insights dashboard",
     company: "Duffel",
     link: "https://duffel.com",
     image: "/selected-work/merchant-insights.png",
   },
   {
-    title: "Gen art minting page",
-    company: "Alba",
-    link: "https://alba.art",
-    image: "/selected-work/project-page.png",
+    title: "Homepage for gen art studio",
+    company: "gm.studio",
+    link: "https://gmstudio.art",
+    image: "/selected-work/gm-studio.png",
   },
   {
     title: "Stays search results with map",
@@ -62,75 +50,85 @@ const projects = [
     link: "https://duffel.com",
     image: "/selected-work/stays-search.png",
   },
+  {
+    title: "Integration guides + API reference",
+    company: "Duffel",
+    link: "https://duffel.com/docs",
+    image: "/selected-work/docs.png",
+  },
+  {
+    title: "Logo + brand design",
+    company: "Dependabot",
+    link: "https://dependabot.com",
+    image: "/selected-work/dependabot-dark-mode.png",
+  },
 ];
 
 export default function Home() {
   return (
     <main className="bg-gray-950">
       {/* Social bar */}
-      <div className="fixed bottom-10 left-2/4 -translate-x-2/4 z-20">
-        <button className="glow-button flex">
-          <span className="flex flex-row justify-center items-center">
-            <Link
-              href="mailto:hey@samwill.is"
-              target="_blank"
-              className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
-            >
-              <Icon
-                name="email"
-                className="fill-gray-500 group-hover:fill-white"
-                size={16}
-              />
-            </Link>
-            <Link
-              href="https://twitter.com/samwill_is"
-              target="_blank"
-              className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
-            >
-              <Icon
-                name="twitter"
-                className="fill-gray-500 group-hover:fill-white"
-                size={16}
-              />
-            </Link>
-            <Link
-              href="https://linkedin.com/in/samjwillis"
-              target="_blank"
-              className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
-            >
-              <Icon
-                name="linkedin"
-                className="fill-gray-500 group-hover:fill-white"
-                size={16}
-              />
-            </Link>
-            <Link
-              href="https://dribbble.com/sjwillis"
-              target="_blank"
-              className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
-            >
-              <Icon
-                name="dribbble"
-                className="fill-gray-500 group-hover:fill-white"
-                size={16}
-              />
-            </Link>
-            <Link
-              href="https://threads.net/@samwill.is"
-              target="_blank"
-              className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
-            >
-              <Icon
-                name="threads"
-                className="fill-gray-500 group-hover:fill-white"
-                size={16}
-              />
-            </Link>
-          </span>
-        </button>
+      <div className="fixed bottom-5 left-2/4 -translate-x-2/4 z-20">
+        <div className="flex flex-row justify-center items-center bg-gray-950/50 backdrop-blur-md rounded-full p-1">
+          <Link
+            href="mailto:hey@samwill.is"
+            target="_blank"
+            className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
+          >
+            <Icon
+              name="email"
+              className="fill-gray-500 group-hover:fill-white"
+              size={16}
+            />
+          </Link>
+          <Link
+            href="https://twitter.com/samwill_is"
+            target="_blank"
+            className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
+          >
+            <Icon
+              name="twitter"
+              className="fill-gray-500 group-hover:fill-white"
+              size={16}
+            />
+          </Link>
+          <Link
+            href="https://linkedin.com/in/samjwillis"
+            target="_blank"
+            className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
+          >
+            <Icon
+              name="linkedin"
+              className="fill-gray-500 group-hover:fill-white"
+              size={16}
+            />
+          </Link>
+          <Link
+            href="https://dribbble.com/sjwillis"
+            target="_blank"
+            className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
+          >
+            <Icon
+              name="dribbble"
+              className="fill-gray-500 group-hover:fill-white"
+              size={16}
+            />
+          </Link>
+          <Link
+            href="https://threads.net/@samwill.is"
+            target="_blank"
+            className="h-8 w-8 flex items-center justify-center rounded-full z-10 group"
+          >
+            <Icon
+              name="threads"
+              className="fill-gray-500 group-hover:fill-white"
+              size={16}
+            />
+          </Link>
+        </div>
       </div>
 
-      <div className="flex flex-col py-6 px-6 gap-16">
+      <div className="flex flex-col py-6 px-6 gap-32">
         {/* Header */}
         <div className="flex flex-row gap-4">
           <Image
@@ -147,13 +145,16 @@ export default function Home() {
         </div>
 
         {/* Hero */}
-        <div className="grid responsive-grid gap-6">
-          <div className="flex flex-col gap-6 2xl:col-start-2">
-            <div className="flex flex-col gap-2">
-              <p className="text-gray-50 text-xl">I design digital products.</p>
+        <div className="grid responsive-grid">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3 max-w-3xl">
+              <p className="text-gray-50 text-xl font-medium">
+                I design digital products.
+              </p>
               <p className="text-gray-500 text-xl">
-                I&apos;ve spent the past 10+ years helping companies translate
-                vision into reality, and design into competitive advantage.
+                Hey, I'm Sam — a london-based product designer who&apos;s spent
+                the past 10+ years helping companies translate vision into
+                reality, and design into competitive advantage.
               </p>
               <p className="text-gray-500 text-xl">
                 I&apos;m currently working at{" "}
@@ -164,8 +165,8 @@ export default function Home() {
                 >
                   Duffel
                 </Link>
-                , where we&apos;re building the future of travel, and on the
-                side making{" "}
+                , where we&apos;re building the future of travel. And on the
+                side I'm building{" "}
                 <Link
                   href="https://alba.art"
                   target="_blank"
@@ -173,14 +174,14 @@ export default function Home() {
                 >
                   Alba
                 </Link>
-                , an open platform for launching generative art on Ethereum.
+                , an open platform for generative art on Ethereum.
               </p>
             </div>
-            <div className="border rounded-full border-gray-900 px-4 py-2 text-sm text-gray-500 inline-block">
-              <div className="flex flex-row gap-3 items-center">
-                <span className="h-2 w-2 bg-red-700 rounded-full"></span>
+            <div className="flex shrink grow basis-auto">
+              <span className="border rounded-full border-gray-900 px-4 py-2 text-sm text-gray-500 flex flex-row items-center justify-start gap-3">
+                <span className="h-2 w-2 bg-red-700 rounded-full" />
                 <span>Unavailable for new projects</span>
-              </div>
+              </span>
             </div>
           </div>
         </div>
@@ -218,8 +219,8 @@ export default function Home() {
         </div>
 
         {/* CV */}
-        <div className="grid responsive-grid">
-          <div className="flex flex-col gap-16 2xl:col-start-2">
+        <div className="flex flex-col w-full max-w-2xl mx-auto">
+          <div className="flex flex-col gap-16">
             <div className="flex flex-col gap-2">
               <p className="title">Projects</p>
               <Link
@@ -499,6 +500,14 @@ export default function Home() {
               </Link>
             </div>
           </div>
+        </div>
+
+        {/* Footer */}
+        <div className="flex flex-col lg:flex-row justify-center items center text-center lg:text-left gap-4 pb-16 lg:pb-2 lg:pt-6 lg:justify-between border-t border-gray-900">
+          <p className="text-sm text-gray-400">© 2023, Sam Willis Design.</p>
+          <p className="text-sm text-gray-400">
+            Built with ❤ (also Tailwind, Vercel & NextJS) by me.
+          </p>
         </div>
       </div>
     </main>
