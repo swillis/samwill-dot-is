72eb78f5683ffba01d33589e59c89eb6af484843
Sam Willis <samueljohnwillis@gmail.com>
2023-08-05 10:18:19 +0100

    animation timings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -171,7 +171,7 @@ function AppearIn({ children }: any) {
         style={{
           transform: isInView ? "none" : "translateY(16px)",
           opacity: isInView ? 1 : 0,
-          transition: "all 0.6s cubic-bezier(0.17, 0.55, 0.55, 1) 0.6s",
+          transition: "all 0.6s cubic-bezier(0.17, 0.55, 0.55, 1) 0.8s",
         }}
       >
         {children}
